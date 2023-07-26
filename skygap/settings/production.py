8ec0757f9994fcfc7eaac01e3f1570d590b31c25
Martin <martin.sayhello@gmail.com>
2023-07-26 16:29:23 -0700

    updated AWS S3

--- skygap/settings/production.py
+++ skygap/settings/production.py
@@ -8,6 +8,9 @@ SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = [config('SERVER')]
 
+AWS_STORAGE_BUCKET_NAME = "skygap-media"
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.amazonaws.com" % AWS_STORAGE_BUCKET_NAME
+
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
